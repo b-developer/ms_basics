ee84ca174a7c37aedfceef018cf8ff84e067647d
b-developer <krvinay365@gmail.com>
2020-05-19 20:56:18 +0530

    initial commit

---
+++ rest-micro-services-basics/src/main/java/com/study/restmicroservicesbasics/RestMicroServicesBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.study.restmicroservicesbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMicroServicesBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMicroServicesBasicsApplication.class, args);
+	}
+
+}
